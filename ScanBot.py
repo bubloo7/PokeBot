bf01097677fae2f645a2de66f8392019fcca4e66
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2021-09-09 21:00:10 -0700

    Made data more readable

--- ScanBot.py
+++ ScanBot.py
@@ -19,26 +19,40 @@ class ScanBot(Player):
     # Prints useful information to the console. Then, makes a random move.
     def choose_move(self, battle):
 
+        print("___________Turn #" + str(battle.turn) + "___________")
+
         # All Available moves
+        print("~~~~~MOVES~~~~~")
         print("Available Switches: ", battle.available_switches)
         print("Available Moves: ", battle.available_moves)
 
         # Active Pokemon
+        print("~~~~~Active Pokemon~~~~~")
+
         print("Opp Pokemon: ", battle.opponent_active_pokemon)
         print("My Pokemon: ", battle.active_pokemon)
 
         # Stats of my Pokemon
+        print("~~~~~My STATS~~~~~")
         print("My level: ", battle.active_pokemon.level)
         print("My Stats: ", battle.active_pokemon.stats)
         print("My Item: ", battle.active_pokemon.item)
         print("My Ability: ", battle.active_pokemon.ability)
 
+        # Stats of Enemy Pokemon
+        print("~~~~~Enemy STATS~~~~~")
+        print("Enemy level: ", battle.opponent_active_pokemon.level)
+        print("Enemy Stats: ", battle.opponent_active_pokemon.stats)
+        print("Enemy Item: ", battle.opponent_active_pokemon.item)
+        print("Enemy Ability: ", battle.opponent_active_pokemon.ability)
 
         # Stat boosts
+        print("~~~~~BOOSTS~~~~~")
         print("My Boosts: ", battle.active_pokemon.boosts)
         print("Enemy Boosts: ", battle.opponent_active_pokemon.boosts)
 
         # The fields
+        print("~~~~~FIELD~~~~~")
         print("Fields: ", battle.fields)
         print("Weather: ", battle.weather)
         print("My Side Conditions: ", battle.side_conditions)
@@ -49,16 +63,17 @@ class ScanBot(Player):
 
 async def main():
     # Insert the username and password of the bot
-    bot_name = ""
-    bot_password = ""
+    bot_name = "bubloo8"
+    bot_password = "Ani2akil"
 
-    player =  ScanBot(
-        player_configuration=PlayerConfiguration(bot_name,bot_password),
+    player = ScanBot(
+        player_configuration=PlayerConfiguration(bot_name, bot_password),
         server_configuration=ShowdownServerConfiguration, start_timer_on_battle_start=True
     )
 
     # Makes the bot open to challenges so you can challenge it
-    await player.accept_challenges(None, 100)
+    # await player.ladder(5)
+    await player.send_challenges("stuppp", n_challenges=10)
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main()) 
+    asyncio.get_event_loop().run_until_complete(main())
